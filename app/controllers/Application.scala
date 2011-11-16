27137fe8064da727a5d0fbb4c34c9788e05a876c
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2011-11-16 22:37:17 +0100

    working insert and list

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,7 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models._
 
 object Application extends Controller {
   
@@ -10,10 +11,18 @@ object Application extends Controller {
   }
   
   def add = Action {
+    val entry = new BlogEntry()
+    entry.title = "Whatever"
+    entry.content = "Blah blah blah"
+    entry.save()
     Ok("Everythink's fine")
   }
   
   def save = Action {
     Ok("goood")
+  }
+  
+  def list = Action {
+    Ok(views.html.entryList(BlogEntry.finder.all()))
   }
 }
