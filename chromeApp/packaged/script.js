b62ad88c83bde56c2477aafdc649be0ab22a5276
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2011-11-27 17:08:07 +0100

    date in chrome app

--- chromeApp/packaged/script.js
+++ chromeApp/packaged/script.js
@@ -8,10 +8,12 @@ document.observe("dom:loaded", function() {
             console.log("dupa");
         
             var art = new Element('article');
+            var pubdate = new Element('time', {pubdate: 'pubdate'}).update(entry.creationDate);
             var header = new Element('h3').update(entry.title);
             var section = new Element('section').update(entry.content);
             
             art.appendChild(header);
+            art.appendChild(pubdate);
             art.appendChild(section);
             $('articles').appendChild(art);
         });
