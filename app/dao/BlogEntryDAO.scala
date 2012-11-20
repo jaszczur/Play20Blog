26f62a6d2067c51c4aaf0f513a202ea448c09bfd
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2012-11-20 21:52:53 +0100

    update chrome app manifests

--- app/dao/BlogEntryDAO.scala
+++ app/dao/BlogEntryDAO.scala
@@ -13,77 +13,67 @@ import domain._
 class BlogEntryDAO extends DAO[Long, BlogEntry] {
   // -- Utils
 
-  implicit def pk2option[ID](pk : Pk[ID]) : Option[ID] = pk.toOption
+  implicit def pk2option[ID](pk: Pk[ID]): Option[ID] = pk.toOption
 
   // -- Parsers
-  
+
   val simpleParser = {
-    def parseDate(s: String) : Date = new Date(java.lang.Long.valueOf(s))
-  
-    get[Pk[Long]]("blog_entry.id") ~ 
-    get[String]("blog_entry.title") ~
-    get[Date]("blog_entry.creation_date") map {
-      case id~title~creationDate => BlogEntry(
-        id, title, "", creationDate, Location(0.0, 0.0, "")
-      )
-    }
+    def parseDate(s: String): Date = new Date(java.lang.Long.valueOf(s))
+
+    get[Pk[Long]]("blog_entry.id") ~
+      get[String]("blog_entry.title") ~
+      get[Date]("blog_entry.creation_date") map {
+        case id ~ title ~ creationDate => BlogEntry(
+          id, title, "", creationDate, Location(0.0, 0.0, ""))
+      }
   }
 
   val fullParser = {
-    def parseDate(s: String) : Date = new Date(java.lang.Long.valueOf(s))
-  
-    get[Pk[Long]]("blog_entry.id") ~ 
-    get[String]("blog_entry.title") ~
-    get[String]("blog_entry.content") ~
-    get[Date]("blog_entry.creation_date") ~
-    get[Double]("location.latitude") ~
-    get[Double]("location.longitude") ~
-    get[String]("location.name") map {
-      case id~title~content~creationDate~latitude~longitude~locName => BlogEntry(
-        id, title, content, creationDate, Location(latitude, longitude, locName)
-      )
-    }
+    def parseDate(s: String): Date = new Date(java.lang.Long.valueOf(s))
+
+    get[Pk[Long]]("blog_entry.id") ~
+      get[String]("blog_entry.title") ~
+      get[String]("blog_entry.content") ~
+      get[Date]("blog_entry.creation_date") ~
+      get[Double]("location.latitude") ~
+      get[Double]("location.longitude") ~
+      get[String]("location.name") map {
+        case id ~ title ~ content ~ creationDate ~ latitude ~ longitude ~ locName => BlogEntry(
+          id, title, content, creationDate, Location(latitude, longitude, locName))
+      }
   }
-  
+
   // -- Queries
-  
+
   override def find(id: Long): Option[BlogEntry] = {
     DB.withConnection { implicit connection =>
       SQL("select * from blog_entry join location on location.id = blog_entry.location_id where blog_entry.id = {id}").on(
-        'id -> id
-      ).as(fullParser *).headOption
+        'id -> id).as(fullParser *).headOption
     }
   }
-  
+
   override def list(): Seq[BlogEntry] = {
     DB.withConnection { implicit connection =>
       SQL("select * from blog_entry order by creation_date desc").as(simpleParser *)
     }
   }
-  
+
   override def save(entry: BlogEntry): BlogEntry = {
     DB.withConnection { implicit connection =>
-      
+
       val id: Long = entry.id.getOrElse {
         SQL("select next value for blog_entry_seq").as(scalar[Long].single)
-      }
-      
-      val formattedDate = {
-        val h2DateFormatter = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-        h2DateFormatter.format(entry.creationDate)
       }
 
       SQL(
         """
           insert into location (id, latitude, longitude, name) values ({id}, {latitude}, {longitude}, {name})
-        """
-      ).on(
-        'id -> id,
-        'latitude -> entry.location.latitude,
-        'longitude -> entry.location.longitude,
-        'name -> entry.location.name
-      ).executeUpdate()
-      
+        """).on(
+          'id -> id,
+          'latitude -> entry.location.latitude,
+          'longitude -> entry.location.longitude,
+          'name -> entry.location.name).executeUpdate()
+
       SQL(
         """
           insert into blog_entry (
@@ -91,17 +81,20 @@ class BlogEntryDAO extends DAO[Long, BlogEntry] {
           ) values (
             {id}, {title}, {content}, {creationDate}, {locationId}
           )
-        """
-      ).on(
-        'id -> id,
-        'title -> entry.title,
-        'content -> entry.content,
-        'creationDate -> formattedDate,
-        'locationId -> id
-      ).executeUpdate()
-      
+        """).on(
+          'id -> id,
+          'title -> entry.title,
+          'content -> entry.content,
+          'creationDate -> formatDate(entry.creationDate),
+          'locationId -> id).executeUpdate()
+
       entry.copy(id = Id(id))
     }
+  }
+
+  private lazy val h2DateFormatter = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+  private def formatDate(date: Date): String = {
+    h2DateFormatter.format(date)
   }
 }
 
